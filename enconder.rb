be1dd3ddea713566698d3557cf9e16d8a3160231
jose heriberto perez magana <joseheribertoperezmagana@heriberto.lan>
2012-12-30 11:28:48 -0600

    Add some coments

--- enconder.rb
+++ enconder.rb
@@ -8,6 +8,7 @@ class Enconder
     @index = []
   end
 
+  #we need to split the string and loop through the array
   def run_through_elements
     string_to_array = @string.split
     string_to_array.each do |element|
@@ -15,6 +16,7 @@ class Enconder
     end
   end
 
+  #in this method we apply the right conditions in each case
   def apply_conditions element
     if new_element? element
       add_new_element element
@@ -24,15 +26,18 @@ class Enconder
 
   end
 
+  #update index array with the position of no repeated elements
   def update_index element
     position = @values.index element
     @index << position
   end
 
+  # we return true if some element already exist in no repeated elements array
   def new_element? element
     !@values.include?(element)
   end
 
+  # only we need to add some element if not exist in values array already
   def add_new_element element
     @values << element
     @index << @values.size - 1
